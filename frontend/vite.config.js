e4834a09fb273225e2328011cbe06fd0ec37c4c5
sosinaesayas <sosinaesayas@gmail.com>
2024-06-07 15:18:29 +0300

    adding dist

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
   server: {
     proxy: {
       "/api": {
-		
         target: "https://chat-app-mern-ndou.onrender.com",
         changeOrigin: true,
         secure: true,
